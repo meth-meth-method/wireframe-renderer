c4e13f75fd3f5b65bee918139790db8d258f970d
Pontus Persson <pontus.alexander@gmail.com>
2017-11-28 20:01:39 +0100

    Remove projected prop.

--- public/Face.js
+++ public/Face.js
@@ -3,6 +3,5 @@ import {Vector} from './Vector.js';
 export class Face {
     constructor(vertices) {
         this.vertices = vertices;
-        this.projected = vertices.map(_ => new Vector());
     }
 }
