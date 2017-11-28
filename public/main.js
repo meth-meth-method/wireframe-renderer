010b785247c51586179ba193885490ea1fb78348
Pontus Persson <pontus.alexander@gmail.com>
2017-11-28 19:07:05 +0100

    Simplify.

--- public/main.js
+++ public/main.js
@@ -8,29 +8,6 @@ import {Vector} from './Vector.js';
 import {transform} from './transform.js';
 import {project} from './project.js';
 
-class Renderer {
-    constructor(canvas) {
-        this.canvas = canvas;
-        this.context = canvas.getContext('2d');
-    }
-
-    clear() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-
-    render(mesh, camera) {
-        transform(mesh);
-
-        for (const face of mesh.faces) {
-            for (const vertex of face.projected) {
-                project(vertex, camera, this.canvas);
-            }
-        }
-
-        drawMesh(mesh, this.context);
-    }
-}
-
 function drawMesh(mesh, context) {
     context.strokeStyle = '#fff';
     mesh.faces.forEach(face => {
@@ -54,8 +31,6 @@ async function main() {
     const canvas = document.querySelector('canvas');
     const context = canvas.getContext('2d');
 
-    const renderer = new Renderer(canvas);
-
     const mesh = await fetch('./model/triangle.json').then(r => r.json()).then(createMesh);
     const camera = new Camera();
 
@@ -63,15 +38,25 @@ async function main() {
 
 
     function loop(time) {
-        //mesh.rotation.y = time / 600;
+        mesh.rotation.y = time / 600;
         //mesh.rotation.x = Math.sin(time / 3000) / 2;
-        render();
+
+        draw();
         requestAnimationFrame(loop);
     }
 
-    function render(time) {
-        renderer.clear();
-        renderer.render(mesh, camera);
+    function draw() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        transform(mesh);
+
+        for (const face of mesh.faces) {
+            for (const vertex of face.projected) {
+                project(vertex, camera, canvas);
+            }
+        }
+
+        drawMesh(mesh, context);
     }
 
     loop();
