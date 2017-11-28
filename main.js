0637f43980bc2f1a678827c7666a375b6124d7e3
Pontus Persson <pontus.alexander@gmail.com>
2017-11-28 18:36:33 +0100

    Decouple.

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ import {Object3d} from './Object3d.js';
 import {Vector} from './Vector.js';
 
 import {createTransformer} from './transform.js';
+import {createProjector} from './project.js';
 
 class Renderer {
     constructor(canvas) {
@@ -45,47 +46,6 @@ function createWireframeDrawer(canvas) {
             context.stroke();
         });
     };
-}
-
-function createProjector(canvas) {
-    function offset(vertex, offset) {
-        vertex.x -= offset.x;
-        vertex.y -= offset.y;
-        vertex.z -= offset.z;
-    }
-
-    function perspective(vertex, fov) {
-        vertex.x /= (vertex.z + fov) * (1 / fov);
-        vertex.y /= (vertex.z + fov) * (1 / fov);
-    }
-
-    function zoom(vertex, factor) {
-        vertex.x *= factor;
-        vertex.y *= factor;
-    }
-
-    function center(vertex, canvas) {
-        vertex.x += canvas.width / 2;
-        vertex.y += canvas.height / 2;
-    }
-
-    function projectVertex(vertex, camera) {
-        offset(vertex, camera.pos);
-
-        perspective(vertex, camera.fov);
-
-        zoom(vertex, ((canvas.width + canvas.height) / 2) / 80);
-
-        center(vertex, canvas);
-    }
-
-    return function projectVertices(mesh, camera) {
-        mesh.faces.forEach(face => {
-            face.projected.forEach((vertex) => {
-                projectVertex(vertex, camera);
-            });
-        });
-    }
 }
 
 async function main() {
---
+++ project.js
@@ -0,0 +1,40 @@
+export function createProjector(canvas) {
+    function offset(vertex, offset) {
+        vertex.x -= offset.x;
+        vertex.y -= offset.y;
+        vertex.z -= offset.z;
+    }
+
+    function perspective(vertex, fov) {
+        vertex.x /= (vertex.z + fov) * (1 / fov);
+        vertex.y /= (vertex.z + fov) * (1 / fov);
+    }
+
+    function zoom(vertex, factor) {
+        vertex.x *= factor;
+        vertex.y *= factor;
+    }
+
+    function center(vertex, canvas) {
+        vertex.x += canvas.width / 2;
+        vertex.y += canvas.height / 2;
+    }
+
+    function projectVertex(vertex, camera) {
+        offset(vertex, camera.pos);
+
+        perspective(vertex, camera.fov);
+
+        zoom(vertex, ((canvas.width + canvas.height) / 2) / 80);
+
+        center(vertex, canvas);
+    }
+
+    return function projectVertices(mesh, camera) {
+        mesh.faces.forEach(face => {
+            face.projected.forEach((vertex) => {
+                projectVertex(vertex, camera);
+            });
+        });
+    }
+}
