561ea4a75d8619799c8b9782bc5bd0727160ad90
Pontus Persson <pontus.alexander@gmail.com>
2017-11-28 19:07:36 +0100

    Remove clone method.

--- public/Vector.js
+++ public/Vector.js
@@ -5,10 +5,6 @@ export class Vector {
         this.z = z;
     }
 
-    clone() {
-        return new Vector(this.x, this.y, this.z);
-    }
-
     copy(vertex) {
         this.x = vertex.x;
         this.y = vertex.y;
