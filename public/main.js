30b7ec989d3dc9a6e425e3bb4148314837b3a6ee
Pontus Persson <pontus.alexander@gmail.com>
2017-11-30 20:52:34 +0100

    Failed perspective.

--- public/main.js
+++ public/main.js
@@ -11,13 +11,53 @@ function drawPolygon(polygon, context, color = '#fff') {
     context.stroke();
 }
 
+function rotate(vertex, rotation) {
+    const sin = {x: 0, y: 0, z: 0};
+    const cos = {x: 0, y: 0, z: 0};
+
+    cos.x = Math.cos(rotation.x);
+    sin.x = Math.sin(rotation.x);
+
+    cos.y = Math.cos(rotation.y);
+    sin.y = Math.sin(rotation.y);
+
+    cos.z = Math.cos(rotation.z);
+    sin.z = Math.sin(rotation.z);
+
+    let temp1, temp2;
+
+    temp1 = cos.x * vertex.y + sin.x * vertex.z;
+    temp2 = -sin.x * vertex.y + cos.x * vertex.z;
+    vertex.y = temp1;
+    vertex.z = temp2;
+
+    temp1 = cos.y * vertex.x + sin.y * vertex.z;
+    temp2 = -sin.y * vertex.x + cos.y * vertex.z;
+    vertex.x = temp1;
+    vertex.z = temp2;
+
+    temp1 = cos.z * vertex.x + sin.z * vertex.y;
+    temp2 = -sin.z * vertex.x + cos.z * vertex.y;
+    vertex.x = temp1;
+    vertex.y = temp2;
+}
+
+const perspective = ({x, y, z}, fov) => ({
+    x: x / (z + fov) * (1 / fov),
+    y: y / (z + fov) * (1 / fov),
+});
+
 const center = ({x, y}, canvas) => ({
     x: x + canvas.width / 2,
-    y: y + canvas.height /2,
+    y: y + canvas.height / 2,
 });
 
 
 async function main() {
+    const camera = {
+        fov: 30,
+    };
+
     const canvas = document.querySelector('canvas');
     const context = canvas.getContext('2d');
 
@@ -33,7 +73,11 @@ async function main() {
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         for (const polygon of mesh) {
-            const transformedPolygon = polygon.map(vertex => center(vertex, canvas));
+            const transformedPolygon = polygon.map(vertex => {
+                rotate(vertex, {x: 1, y: 0, z: 0});
+                vertex = perspective(vertex, 0.2);
+                return center(vertex, canvas);
+            });
             console.log(transformedPolygon);
             drawPolygon(transformedPolygon, context, '#fff');
         }
