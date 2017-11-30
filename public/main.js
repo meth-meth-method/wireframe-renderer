7acb0ebb723ab7d7909f9843b6009dfee74e963f
Pontus Persson <pontus.alexander@gmail.com>
2017-11-30 20:59:58 +0100

    WTH

--- public/main.js
+++ public/main.js
@@ -11,40 +11,9 @@ function drawPolygon(polygon, context, color = '#fff') {
     context.stroke();
 }
 
-function rotate(vertex, rotation) {
-    const sin = {x: 0, y: 0, z: 0};
-    const cos = {x: 0, y: 0, z: 0};
-
-    cos.x = Math.cos(rotation.x);
-    sin.x = Math.sin(rotation.x);
-
-    cos.y = Math.cos(rotation.y);
-    sin.y = Math.sin(rotation.y);
-
-    cos.z = Math.cos(rotation.z);
-    sin.z = Math.sin(rotation.z);
-
-    let temp1, temp2;
-
-    temp1 = cos.x * vertex.y + sin.x * vertex.z;
-    temp2 = -sin.x * vertex.y + cos.x * vertex.z;
-    vertex.y = temp1;
-    vertex.z = temp2;
-
-    temp1 = cos.y * vertex.x + sin.y * vertex.z;
-    temp2 = -sin.y * vertex.x + cos.y * vertex.z;
-    vertex.x = temp1;
-    vertex.z = temp2;
-
-    temp1 = cos.z * vertex.x + sin.z * vertex.y;
-    temp2 = -sin.z * vertex.x + cos.z * vertex.y;
-    vertex.x = temp1;
-    vertex.y = temp2;
-}
-
 const perspective = ({x, y, z}, fov) => ({
-    x: x / (z + fov) * (1 / fov),
-    y: y / (z + fov) * (1 / fov),
+    x: x / ((z + fov) * (1 / fov)),
+    y: y / ((z + fov) * (1 / fov)),
 });
 
 const center = ({x, y}, canvas) => ({
@@ -74,8 +43,7 @@ async function main() {
 
         for (const polygon of mesh) {
             const transformedPolygon = polygon.map(vertex => {
-                rotate(vertex, {x: 1, y: 0, z: 0});
-                vertex = perspective(vertex, 0.2);
+                vertex = perspective(vertex, 30);
                 return center(vertex, canvas);
             });
             console.log(transformedPolygon);
