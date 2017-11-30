c297f3d96116e2faddc2f25d414c9f3b668a722f
Pontus Persson <pontus.alexander@gmail.com>
2017-11-30 20:29:11 +0100

    Remove everything.

--- public/Polygon.js
+++
@@ -1,9 +0,0 @@
-import {Vector} from './Vector.js';
-
-export class Polygon extends Array {
-    constructor(vertices) {
-        super();
-        this.color = undefined;
-        this.push(...vertices);
-    }
-}
