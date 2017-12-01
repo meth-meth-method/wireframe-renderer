a8d14a1cb216228e8677cdf2e359b8a117692e8c
Pontus Persson <pontus.alexander@gmail.com>
2017-12-01 21:13:17 +0100

    Setup drawPolygon function.

--- public/main.js
+++ public/main.js
@@ -9,6 +9,21 @@ function toPoint([x, y]) {
     return {x, y};
 }
 
+function drawPolygon(polygon, context, color = '#fff') {
+    context.strokeStyle = color;
+
+    context.beginPath();
+
+    const first = polygon[0];
+    context.moveTo(first.x, first.y);
+    for (const point of polygon) {
+        context.lineTo(point.x, point.y);
+    }
+    context.lineTo(first.x, first.y);
+
+    context.stroke();
+}
+
 async function main() {
     const canvas = document.querySelector('canvas');
     const context = canvas.getContext('2d');
@@ -16,6 +31,8 @@ async function main() {
     console.log(square);
     const polygon = square.map(toPoint);
     console.log(polygon);
+
+    drawPolygon(polygon, context);
 }
 
 main();
