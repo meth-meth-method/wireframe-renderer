ef99a34809723a0a3d709e42d5719adff0f8a45a
Pontus Persson <pontus.alexander@gmail.com>
2017-12-01 21:08:54 +0100

    Start with a square data and convert to a polygon.

--- public/main.js
+++ public/main.js
@@ -1,71 +1,24 @@
-function drawPolygon(polygon, context, color = '#fff') {
-    const first = polygon[0];
+const square = [
+    [-50, -50],
+    [ 50, -50],
+    [ 50,  50],
+    [-50,  50]
+];
 
-    context.strokeStyle = color;
-    context.beginPath();
-    context.moveTo(first.x, first.y);
-    polygon.forEach((vert, index) => {
-        context.lineTo(vert.x, vert.y);
-    });
-    context.lineTo(first.x, first.y);
-    context.stroke();
+function toVector(values) {
+    return {
+        x: values[0],
+        y: values[1],
+    };
 }
-
-const perspective = (v, c) => {
-    const distance = Math.abs(v.z - c.z);
-    //const magnitude = (v.z + fov) * (2 / fov);
-    //v.x = v.x * (focalLength / v.z);
-    //v.y = v.y * (focalLength / v.z);
-    v.x = distance * (v.x - c.x) / (v.z + distance) + c.x;
-    v.y = distance * (v.y - c.y) / (v.z + distance) + c.y;
-};
-
-const center = (v, canvas) => {
-    v.x += canvas.width / 2;
-    v.y += canvas.height / 2;
-};
-
-const zoom = (v, factor) => {
-    v.x *= factor;
-    v.y *= factor;
-}
-
 
 async function main() {
-    const camera = {
-        fov: 30,
-        pos: {x: 0, y: 0, z: 200},
-    };
-
     const canvas = document.querySelector('canvas');
     const context = canvas.getContext('2d');
 
-    const mesh = await fetch('./model/cube.json')
-        .then(respons => respons.json())
-        .then(mesh => {
-            return mesh.map(poly => {
-                return poly.map(([x, y, z]) => ({x, y, z}));
-            });
-        });
-
-    function draw() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-
-        for (const polygon of mesh) {
-            const transformedPolygon = polygon.map(vertIn => {
-                const vertex = {...vertIn};
-                perspective(vertex, camera.pos);
-                //zoom(vertex, 1);
-                center(vertex, canvas);
-                return vertex;
-            });
-            console.log(transformedPolygon);
-            drawPolygon(transformedPolygon, context, '#fff');
-        }
-
-    }
-
-    draw();
+    console.log(square);
+    const polygon = square.map(toVector);
+    console.log(polygon);
 }
 
 main();
