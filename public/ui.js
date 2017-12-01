8440236975398f902511781f0736368e76f36ec4
Pontus Persson <pontus.alexander@gmail.com>
2017-12-01 22:05:10 +0100

    Add UI module.

---
+++ public/ui.js
@@ -0,0 +1,22 @@
+function findProp(address, target) {
+    const trail = address.split('.');
+    const prop = trail.pop();
+    const object = trail.reduce((object, prop) => {
+        return object[prop];
+    }, target);
+    return {object, prop}
+}
+
+export function control(targets, callback) {
+    const inputs = document.querySelectorAll('.controls input');
+    [...inputs].forEach(input => {
+        const {prop, object} = findProp(input.name, targets);
+        input.addEventListener('input', event => {
+            const value = parseFloat(event.target.value);
+            object[prop] = value;
+            console.log('Setting %s to %f', prop, value);
+            callback();
+        });
+        input.value = object[prop];
+    });
+}
