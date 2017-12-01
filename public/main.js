a9a3cea50987a338421607416629997b814ff9f6
Pontus Persson <pontus.alexander@gmail.com>
2017-12-01 07:21:49 +0100

    Camera

--- public/main.js
+++ public/main.js
@@ -11,20 +11,30 @@ function drawPolygon(polygon, context, color = '#fff') {
     context.stroke();
 }
 
-const perspective = ({x, y, z}, fov) => ({
-    x: x / ((z + fov) * (1 / fov)),
-    y: y / ((z + fov) * (1 / fov)),
-});
+const perspective = (v, c) => {
+    const distance = Math.abs(v.z - c.z);
+    //const magnitude = (v.z + fov) * (2 / fov);
+    //v.x = v.x * (focalLength / v.z);
+    //v.y = v.y * (focalLength / v.z);
+    v.x = distance * (v.x - c.x) / (v.z + distance) + c.x;
+    v.y = distance * (v.y - c.y) / (v.z + distance) + c.y;
+};
 
-const center = ({x, y}, canvas) => ({
-    x: x + canvas.width / 2,
-    y: y + canvas.height / 2,
-});
+const center = (v, canvas) => {
+    v.x += canvas.width / 2;
+    v.y += canvas.height / 2;
+};
+
+const zoom = (v, factor) => {
+    v.x *= factor;
+    v.y *= factor;
+}
 
 
 async function main() {
     const camera = {
         fov: 30,
+        pos: {x: 0, y: 0, z: 200},
     };
 
     const canvas = document.querySelector('canvas');
@@ -42,9 +52,12 @@ async function main() {
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         for (const polygon of mesh) {
-            const transformedPolygon = polygon.map(vertex => {
-                vertex = perspective(vertex, 30);
-                return center(vertex, canvas);
+            const transformedPolygon = polygon.map(vertIn => {
+                const vertex = {...vertIn};
+                perspective(vertex, camera.pos);
+                //zoom(vertex, 1);
+                center(vertex, canvas);
+                return vertex;
             });
             console.log(transformedPolygon);
             drawPolygon(transformedPolygon, context, '#fff');
