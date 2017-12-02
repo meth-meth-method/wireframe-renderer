d5a13ae8e8b01574cb5655a8e68e3f2631b9580e
Pontus Alexander <pontus.alexander@gmail.com>
2017-12-02 16:35:54 +0100

    Add rotation function and rotation property to Mesh.

--- public/mesh.js
+++ public/mesh.js
@@ -18,10 +18,40 @@ function offset(point, position) {
     point.z += position.z;
 }
 
+function rotate(point, rotation) {
+    const sin = new Vec(
+        Math.sin(rotation.x),
+        Math.sin(rotation.y),
+        Math.sin(rotation.z));
+
+    const cos = new Vec(
+        Math.cos(rotation.x),
+        Math.cos(rotation.y),
+        Math.cos(rotation.z));
+
+    let temp1, temp2;
+
+    temp1 = cos.x * point.y + sin.x * point.z;
+    temp2 = -sin.x * point.y + cos.x * point.z;
+    point.y = temp1;
+    point.z = temp2;
+
+    temp1 = cos.y * point.x + sin.y * point.z;
+    temp2 = -sin.y * point.x + cos.y * point.z;
+    point.x = temp1;
+    point.z = temp2;
+
+    temp1 = cos.z * point.x + sin.z * point.y;
+    temp2 = -sin.z * point.x + cos.z * point.y;
+    point.x = temp1;
+    point.y = temp2;
+}
+
 class Mesh {
     constructor(polygons) {
         this.polygons = polygons;
         this.position = new Vec();
+        this.rotation = new Vec();
     }
 
     *[Symbol.iterator] () {
