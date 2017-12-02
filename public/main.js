dc94017e371dc563bf5a391cee171206d16e1a5f
Pontus Alexander <pontus.alexander@gmail.com>
2017-12-02 11:22:27 +0100

    Add rotate function and rotation variable.

--- public/main.js
+++ public/main.js
@@ -3,6 +3,39 @@ import {control} from './ui.js';
 import {drawPolygon} from './draw.js';
 import {Vec, toPoint} from './math.js';
 
+
+function rotate(point, rotation) {
+    const sin = new Vec();
+    const cos = new Vec();
+
+    cos.x = Math.cos(rotation.x);
+    sin.x = Math.sin(rotation.x);
+
+    cos.y = Math.cos(rotation.y);
+    sin.y = Math.sin(rotation.y);
+
+    cos.z = Math.cos(rotation.z);
+    sin.z = Math.sin(rotation.z);
+
+    let temp1, temp2;
+
+    temp1 = cos.x * point.y + sin.x * point.z;
+    temp2 = -sin.x * point.y + cos.x * point.z;
+    point.y = temp1;
+    point.z = temp2;
+
+    temp1 = cos.y * point.x + sin.y * point.z;
+    temp2 = -sin.y * point.x + cos.y * point.z;
+    point.x = temp1;
+    point.z = temp2;
+
+    temp1 = cos.z * point.x + sin.z * point.y;
+    temp2 = -sin.z * point.x + cos.z * point.y;
+    point.x = temp1;
+    point.y = temp2;
+}
+
+
 function toCenter(point, canvas) {
     point.x += canvas.width / 2;
     point.y += canvas.height / 2;
@@ -32,10 +65,14 @@ async function main() {
         return polygon;
     });
 
+    const rotation = new Vec(0, 0, 0);
+
+
     const camera = {
         pos: new Vec(0, 0, -500),
         zoom: 500,
     };
+
 
     function draw() {
         context.clearRect(0, 0, canvas.width, canvas.height);
@@ -44,6 +81,7 @@ async function main() {
             const projectedPolygon = polygon
                 .map(point => point.clone())
                 .map(point => {
+                    rotate(point, rotation)
                     offset(point, camera.pos);
                     perspective(point, camera);
                     toCenter(point, canvas);
@@ -62,7 +100,7 @@ async function main() {
 
     animate();
 
-    control({camera});
+    control({camera, mesh: {rotation}});
 }
 
 main();
