5e26277e480d03f351a4f6d515a8c99f4d2eb541
Pontus Alexander <pontus.alexander@gmail.com>
2017-12-02 15:46:56 +0100

    Create `createMesh` function to convey steps.

--- public/main.js
+++ public/main.js
@@ -8,10 +8,14 @@ function toPolygon(shape) {
     return shape.map(toPoint);
 }
 
+function createMesh(model) {
+    return model.map(toPolygon);
+}
+
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
 console.log(square);
 
-const mesh = square.map(toPolygon);
+const mesh = createMesh(square);
 console.log(mesh);
