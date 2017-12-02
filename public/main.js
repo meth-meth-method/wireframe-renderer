15b80569a259f44e51d268b5352aa3c233a588b5
Pontus Alexander <pontus.alexander@gmail.com>
2017-12-02 15:09:31 +0100

    Add `toPoint` function and convert model to mesh.
    Nomenclature:
    `toPoint()` converts Coord to Point.
    Coord is a list, and Point has values named x, y, z.
    A processed model becomes a Mesh.
    
    A processed model is referred to
    Mesh > Polygon > Point.

--- public/main.js
+++ public/main.js
@@ -1,6 +1,17 @@
 import {square} from './models.js';
 
+function toPoint(values) {
+    return {
+        x: values[0],
+        y: values[1],
+        z: values[2],
+    };
+}
+
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
 
 console.log(square);
+
+const mesh = square.map(shape => shape.map(toPoint));
+console.log(mesh);
